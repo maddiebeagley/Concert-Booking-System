9e9788f822c58c55b2781f54b0554cbc02ac40e3
maddiebeagley <beagleymaddie@gmail.com>
2018-09-15 16:21:25 +1200

    added news item class to domain model

--- softeng325-concert/softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/jpa/NewsItem.java
+++ softeng325-concert/softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/jpa/NewsItem.java
@@ -1,4 +1,45 @@
 package nz.ac.auckland.concert.service.domain.jpa;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class NewsItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "newsItemId")
+    private Long _newsItemId;
+
+    @Column(name = "title")
+    private String _title;
+
+    @Column(name = "message")
+    private String _message;
+
+    @Column(name = "publicationDate")
+    private LocalDateTime _publicationDate;
+
+    public NewsItem(){};
+
+    public NewsItem(String title, String message, LocalDateTime publicationDate){
+        _title = title;
+        _message = message;
+        _publicationDate = publicationDate;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public String getMessage() {
+        return _message;
+    }
+
+    public LocalDateTime getPublicationDate() {
+        return _publicationDate;
+    }
 }
